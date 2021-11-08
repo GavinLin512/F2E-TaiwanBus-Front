b8a46c072d199d07d705d342911bbf986a722cfb
nick2258 <76420924+nick2258@users.noreply.github.com>
2021-11-09 01:09:29 +0800

    新增點擊遮罩關閉選單效果

--- js/nav.js
+++ js/nav.js
@@ -2,29 +2,32 @@ const menuBtn = document.querySelector(".menu-btn");
 const hamburgermenu = document.querySelector(".hamburger-menu");
 const bg = document.querySelector(".bg");
 
-let menuOpen = false;
 menuBtn.addEventListener("click", () => {
-    if (!menuOpen) {
+    if (menuBtn.classList.contains("open")) {
+        remove();
+    } else {
         show();
         hamburgermenu.classList.remove("remove");
         hamburgermenu.classList.add("show");
         bg.classList.remove("remove");
         bg.classList.add("show");
+    }
+});
 
-        menuOpen = true;
-    } else {
-        show();
-        hamburgermenu.classList.remove("show");
-        hamburgermenu.classList.add("remove");
-        bg.classList.remove("show");
-        bg.classList.add("remove");
-
-        menuOpen = false;
+bg.addEventListener("click", () => {
+    if (menuBtn.classList.contains("open")) {
+        remove();
     }
 });
 
 function show() {
-    // menuBtn.classList.toggle("open");
-    hamburgermenu.classList.toggle("show");
-    // bg.classList.toggle("show");
+    menuBtn.classList.toggle("open");
+}
+
+function remove() {
+    menuBtn.classList.toggle("open");
+    hamburgermenu.classList.remove("show");
+    hamburgermenu.classList.add("remove");
+    bg.classList.remove("show");
+    bg.classList.add("remove");
 }
